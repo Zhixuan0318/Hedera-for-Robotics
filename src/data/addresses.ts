e76ecab05021b456684864b3dba6125968c656f0
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2024-12-07 15:27:25 +0800

    add data

---
+++ src/data/addresses.ts
@@ -0,0 +1,9 @@
+const addresses: { [key: string]: `0x${string}` } = {
+    owner: '0x6F01939f87C681b4FC0a36127Ae0Bd7B4b5B9F40',
+    shop: '0xCf670989577b0a0EbD93eE2104cABbF9bFFdfCe7',
+    warehouse: '0x2Fa30a9845Ae90914594882def5718C6B2420693',
+    picker: '0xfce3D945Ef4e749e2AEE405eAD006715eE6884c8',
+    packer: '0xF2CC729Ab0AE4883d71d1dA6bFbE48f8c78b20bB',
+    deliverer: '0x089645AECD9d00016Da8C30F74868CE2E3E7dA4f',
+};
+export default addresses;
---
+++ src/data/contract-ids.ts
@@ -0,0 +1,12 @@
+export default {
+    '3plTopic': '0.0.5180824',
+    recipientTopic: '0.0.5180826',
+    tokenIdGreen: '0.0.5172085',
+    tokenIdPurple: '0.0.5172087',
+    tokenIdBlue: '0.0.5172088',
+    shop: '0.0.5172114',
+    warehouse: '0.0.5172115',
+    picker: '0.0.5172117',
+    packer: '0.0.5172119',
+    deliverer: '0.0.5172121',
+};
